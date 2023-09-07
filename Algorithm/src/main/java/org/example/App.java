53c15732be85ddcec15c1b4628c62903805d2c6f
lsh <3040114965@qq.com>
2023-09-07 22:19:03 +0800

    1

--- Algorithm/src/main/java/org/example/App.java
+++ Algorithm/src/main/java/org/example/App.java
@@ -1,12 +1,81 @@
 package org.example;
 
-import java.util.Stack;
-
-
+import java.util.*;
 
 public class App {
     public static void main(String[] args) {
 
     }
 
+    public boolean isNumber(String s) {
+        char[] ch = s.toCharArray();
+        int size = ch.length;
+        for (int i = 0; i < size; i++) {
+            while (ch[i] == ' ') i++;
+            while (ch[i] >= '0' && ch[i] <= '9')
+                i++;
+            boolean after = ch[i + 1] >= '0' && ch[i + 1] <= '9';
+            if ((ch[i] == '-' || ch[i] == '+')) {
+                if (i == size - 1) return false;
+                if (!after) {
+                    return false;
+                }
+            }
+
+            if ((ch[i] == '.' || ch[i] == 'e' || ch[i] == 'E')) {
+                if (i == 0) return false;
+                boolean before = ch[i - 1] >= '0' && ch[i - 1] <= '9';
+                if (ch[i] == '.') {
+                    if (!before || !after) return false;
+                } else {
+                    if (!(before && after)) {
+                        if (ch[i] != '-') {
+                            return false;
+                        } else {
+                            if (!after)
+                                return false;
+                        }
+                    }
+                }
+
+            }
+        }
+        return true;
+    }
+
+}
+
+class MinStack {
+    Stack<Integer> stack;
+    Stack<Integer> minStack;
+
+    /**
+     * initialize your data structure here.
+     */
+    public MinStack() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }
+
+    public void push(int x) {
+        stack.push(x);
+        if (minStack.isEmpty() || x <= minStack.peek()){
+            minStack.push(x);
+        }
+    }
+
+    public void pop() {
+        int t = stack.pop();
+        if (minStack.peek() == t){
+            minStack.pop();
+        }
+    }
+
+    public int top() {
+       return stack.peek();
+    }
+
+    public int min() {
+        return minStack.peek();
+    }
 }
--- Algorithm/src/test/java/org/example/AppTest.java
+++ Algorithm/src/test/java/org/example/AppTest.java
@@ -1,38 +1,25 @@
 package org.example;
 
-import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+public class AppTest {
+    @Test
+    public void test01() {
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        System.out.println(list);
+        list.remove(list.size() - 1);
+        System.out.println(list);
+        System.out.println(list.get(list.size() - 1));
     }
 }
