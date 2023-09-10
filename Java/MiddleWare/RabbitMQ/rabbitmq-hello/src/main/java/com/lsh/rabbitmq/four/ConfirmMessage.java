5b2f4428edfdc46d75b678a2c2dc833c222dcc72
lsh <3040114965@qq.com>
2023-09-10 16:02:38 +0800

    1

--- Java/MiddleWare/RabbitMQ/rabbitmq-hello/src/main/java/com/lsh/rabbitmq/four/ConfirmMessage.java
+++ Java/MiddleWare/RabbitMQ/rabbitmq-hello/src/main/java/com/lsh/rabbitmq/four/ConfirmMessage.java
@@ -4,23 +4,26 @@ import com.lsh.rabbitmq.utils.RabbitUtils;
 import com.rabbitmq.client.Channel;
 
 import java.util.UUID;
+import java.util.concurrent.ConcurrentNavigableMap;
+import java.util.concurrent.ConcurrentSkipListMap;
 
 /**
  * 发布确认模式
  * 使用的时间 比较哪种方式最好
  * 1.单个确认       -->     1575ms
  * 2.批量确认       -->     171ms
- * 3.异步批量确认
+ * 3.异步批量确认    -->     100ms
  */
 public class ConfirmMessage {
     private static final int MESSAGE_COUNT = 1000;
 
     public static void main(String[] args) throws Exception {
         // 1.单个确认
-        publicMessageIndividually();
+//        publicMessageIndividually();
         // 2.批量确认
-        publishMessageBatch();
+//        publishMessageBatch();
         // 3.异步批量确认
+        publicMessageAsync();
     }
 
     // 单个确认
@@ -78,5 +81,47 @@ public class ConfirmMessage {
 
         long end = System.currentTimeMillis();
         System.out.println("发布" + MESSAGE_COUNT + "个批量确认消息，耗时" + (end - begin) + "ms");
+    }
+
+    // 异步确认发布
+    public static void publicMessageAsync() throws Exception {
+        Channel channel = RabbitUtils.getChannel();
+        String queueName = UUID.randomUUID().toString();
+        channel.queueDeclare(queueName, true, false, false, null);
+
+        channel.confirmSelect();
+        /*
+         * 线程安全有序的一个哈希表，适用于高并发的情况下
+         */
+        ConcurrentSkipListMap<Long, String> outstandingConfirms = new ConcurrentSkipListMap<>();
+
+        // 准备消息的监听器，监听哪些消息成功了，那些消息失败了
+        // 1.监听成功的 deliveryTag消息的编号 multiple是否为批量确认
+        // 2.监听失败的
+        channel.addConfirmListener((deliveryTag, multiple) -> {
+            // ->2:删除已经确认的消息
+            if (multiple) {
+                // headMap是严格返回key小于deliveryTag的集合
+                ConcurrentNavigableMap<Long, String> confirmed = outstandingConfirms.headMap(deliveryTag);
+                confirmed.clear();
+            } else {
+                outstandingConfirms.remove(deliveryTag);
+            }
+            System.out.println("确认的消息：" + deliveryTag);
+        }, (deliveryTag, multiple) -> {
+            //3:打印以下未确认的消息
+            String message = outstandingConfirms.get(deliveryTag);
+            System.out.println("未确认的消息：" + message);
+            System.out.println("未确认的Tag：" + deliveryTag);
+        }); // 异步通知
+        long begin = System.currentTimeMillis();
+        for (int i = 0; i < MESSAGE_COUNT; i++) {
+            String message = i + "";
+            //->1:记录所有要发送的消息
+            outstandingConfirms.put(channel.getNextPublishSeqNo(), message);
+            channel.basicPublish("", queueName, null, message.getBytes());
+        }
+        long end = System.currentTimeMillis();
+        System.out.println("发布" + MESSAGE_COUNT + "个异步确认消息，耗时" + (end - begin) + "ms");
     }
 }
